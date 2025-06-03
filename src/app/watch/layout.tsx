32c150c96fba4732365b8d553e5231045fcc88f5
artt1c <roma.myalo000@gmail.com>
2025-06-04 02:19:21 +0300

    header

---
+++ src/app/watch/layout.tsx
@@ -0,0 +1,21 @@
+import React, {ReactNode} from 'react';
+import AsideNavigation from "@/components/aside-navigation/AsideNavigation";
+
+const WatchLayout = ({
+  children
+}: Readonly<{
+  children:ReactNode;
+}>) => {
+  return (
+    <div className="flex">
+      <div className="flex-1">
+        <AsideNavigation />
+      </div>
+      <div className="flex-4">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default WatchLayout;
