7d484ecfe175aa5e5844754729cf22c3b880d49a
artt1c <roma.myalo000@gmail.com>
2025-06-05 01:41:48 +0300

    account menu

--- src/components/acount-menu/AccountMenu.tsx
+++ src/components/acount-menu/AccountMenu.tsx
@@ -5,15 +5,27 @@ import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 import {Button} from "@/components/ui/button";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
+import {ChevronDown, User} from "lucide-react";
 
 export function AccountMenu() {
 
   return (
     <Popover>
-      <PopoverTrigger asChild className="h-12 w-40">
-        <Button variant="outline">Open popover</Button>
+      <PopoverTrigger asChild className="h-12 w-45 rounded-2xl !bg-(--interactive) cursor-pointer">
+        <Button variant="outline" className="!p-1 justify-between gap-2">
+          <div className="border-2 size-10 rounded-xl">
+            {
+              1!==1 ? <div></div> : <User className="size-full p-1"/>
+            }
+          </div>
+          <div className="flex-1 text-left h-10">
+            <p className="font-bold col-(--foreground-main) text-xl leading-none">Guest</p>
+            <p className="text-yellow-300">status</p>
+          </div>
+          <ChevronDown className="size-6" strokeWidth="3"/>
+        </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-80">
+      <PopoverContent className="w-80 mt-1 bg-(--interactive)" align="end">
         <div className="grid gap-4">
           <div className="space-y-2">
             <h4 className="leading-none font-medium">Dimensions</h4>
