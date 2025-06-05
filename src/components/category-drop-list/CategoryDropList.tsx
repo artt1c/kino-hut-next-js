189365b76a617c880bffc2244d9596624e8370d1
artt1c <roma.myalo000@gmail.com>
2025-06-05 13:45:38 +0300

    fix add Suspense

--- src/components/category-drop-list/CategoryDropList.tsx
+++ src/components/category-drop-list/CategoryDropList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {useState} from "react";
+import React, {Suspense, useState} from "react";
 import {useSearchParams} from "next/navigation";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 import {Button} from "@/components/ui/button";
@@ -31,41 +31,43 @@ const CategoryDropList = () => {
   const [open, setOpen] = useState<boolean>(false)
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-37 h-12 justify-between !bg-(--interactive) font-bold text-lg !px-5 !py-3 capitalize rounded-2xl border-0"
-        >
-          {type}
-          <ChevronDown className="opacity-70 size-5 !mt-1" strokeWidth="3"/>
-        </Button>
-      </PopoverTrigger>
+    <Suspense>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-37 h-12 justify-between !bg-(--interactive) font-bold text-lg !px-5 !py-3 capitalize rounded-2xl border-0"
+          >
+            {type}
+            <ChevronDown className="opacity-70 size-5 !mt-1" strokeWidth="3"/>
+          </Button>
+        </PopoverTrigger>
 
-      <PopoverContent className="w-37 !p-0">
-        <Command className="bg-(--interactive)">
-          <CommandList>
-            <CommandGroup>
-              {categories.map((category, id) => (
-                <Link href={params+category.value} key={id}>
-                  <CommandItem
-                    className={cn("text-lg font-bold capitalize", type === category.value ? 'hidden' : '')}
-                    value={category.value}
-                    onSelect={() => {
-                      setOpen(false)
-                    }}
-                  >
-                    {category.value}
-                  </CommandItem>
-                </Link>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+        <PopoverContent className="w-37 !p-0">
+          <Command className="bg-(--interactive)">
+            <CommandList>
+              <CommandGroup>
+                {categories.map((category, id) => (
+                  <Link href={params+category.value} key={id}>
+                    <CommandItem
+                      className={cn("text-lg font-bold capitalize", type === category.value ? 'hidden' : '')}
+                      value={category.value}
+                      onSelect={() => {
+                        setOpen(false)
+                      }}
+                    >
+                      {category.value}
+                    </CommandItem>
+                  </Link>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </Suspense>
   );
 };
 
